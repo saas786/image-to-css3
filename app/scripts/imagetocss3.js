8b6d0bccdecb1ef02d8661f3db598b7e01ec273d
romainberger <signup@romainberger.com>
2012-10-20 15:12:35 +0200

    Refactored the code as an class to make it easier to use

--- app/scripts/imagetocss3.js
+++ app/scripts/imagetocss3.js
@@ -1,104 +1,124 @@
-var pixelSize,
-	pixelSizeValue = document.querySelector('#pixelSizeValue');
-
-document.querySelector('input[type=range]').addEventListener('change', function() {
-	pixelSizeValue.innerHTML = this.value;
-});
-
-document.querySelector('input[type=file]').addEventListener('change', function(e) {
-	imageToCss3(this.files);
-});
+/**
+ *	Image to CSS converter
+ *
+ *	@author Romain Berger <romain@romainberger.com>
+ */
 
 
-function imageToCss3(files) {
-	pixelSize = document.querySelector('input[type=range]').value;
-	document.querySelector('#input').innerHTML = '<p>Processing ...</p>';
-	// read the image without uploading it
-    var reader = new FileReader();
-    reader.onload = pixelize;
-    reader.readAsDataURL(files[0]);
-}
+var ImageToCSS3 = function(img) {
+	this.img = img;
+	this.pixelSize = 1;
+	this.element;
+	this.css = '';
+	this.colors = new Array();
+	this.width;
+	this.height;
 
-function pixelize(e) {
-	var img = new Image;
-	img.src = e.target.result;
+	/**
+	 *	Set the quality of the output. Default to 1 (same quality as the source image)
+	 *
+	 *	@param integer quality
+	 */
+	this.setQuality = function(quality) {
+		this.pixelSize = quality;
+	}
 
-	var canvas = document.createElement('canvas');
-	var ctx = canvas.getContext('2d');
+	/**
+	 *	Returns a div containing the image created with css3
+	 *
+	 *	@param cb Callback function
+	 */
+	this.createElement = function(cb) {
+		var self = this;
 
-	img.onload = function() {
-		var width = img.width,
-			height = img.height;
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
 
-		canvas.width = width;
-		canvas.height = height;
+		this.img.onload = function() {
+			self.width = img.width,
+			self.height = img.height;
 
-		ctx.drawImage(img, 0, 0, width, height);
+			canvas.width  = self.width;
+			canvas.height = self.height;
 
-		// array of array to store the colors
-		var colors = new Array(),
-			maxX = width / pixelSize,
-			maxY = height / pixelSize;
+			ctx.drawImage(img, 0, 0, self.width, self.height);
 
-		// two nested loops ? This is slow as heck
+			var	maxX = self.width / self.pixelSize,
+				maxY = self.height / self.pixelSize;
+
+			// two nested loops ? This is slow as heck
+			for (var x = 0; x < maxX; x++) {
+				self.colors[x] = new Array();
+				for (var y = 0; y < maxY; y++) {
+					var imageData = ctx.getImageData((x * self.pixelSize), (y * self.pixelSize), self.pixelSize, self.pixelSize);
+					var data = imageData.data;
+					var alpha = data[3] == 0 ? 0 : 1;
+					var color = 'rgba('+data[0]+','+data[1]+','+data[2]+','+alpha+')';
+					self.colors[x][y] = color;
+				}
+			}
+
+			self.generateCSS();
+			cb(self.element);
+		}
+	}
+
+	this.generateCSS = function() {
+		var	maxX = this.width / this.pixelSize,
+			maxY = this.height / this.pixelSize;
+
+		var wrapper = document.createElement('div');
+		var mainPixel = document.createElement('div');
+		wrapper.appendChild(mainPixel);
+		wrapper.setAttribute('style', 'width: '+this.width+'px; height: '+this.height+'px;');
+
+		var boxShadowString = '',
+			boxShadowExport = '';
+
+		// once again two nested loops. This sucks.
 		for (var x = 0; x < maxX; x++) {
-			colors[x] = new Array();
 			for (var y = 0; y < maxY; y++) {
-				var imageData = ctx.getImageData((x * pixelSize), (y * pixelSize), pixelSize, pixelSize);
-				var data = imageData.data;
-				var alpha = data[3] == 0 ? 0 : 1;
-				var color = 'rgba('+data[0]+','+data[1]+','+data[2]+','+alpha+')';
-				colors[x][y] = color;
+				var boxShadow = (x * pixelSize) + 'px ' + (y * pixelSize) + 'px 0 ' + this.colors[x][y];
+				boxShadow += ',';
+				boxShadowString += boxShadow;
+				boxShadowExport += boxShadow+'\n\t\t\t';
 			}
 		}
+		// remove the last comma
+		boxShadowString = boxShadowString.slice(0, -1);
+		boxShadowExport = boxShadowExport.slice(0, -5);
 
-		generateCss(colors, maxX, maxY);
-		$('#output').css('height', height);
+		document.querySelector('#input').style.display = 'none';
+
+		// apply the css
+		// the first color is the background of the element
+		var style = 'width: '+this.pixelSize+'px; height: '+this.pixelSize+'px; background: '+this.colors[0][0]+';';
+			style += 'box-shadow: '+boxShadowString;
+		mainPixel.setAttribute('style', style);
+
+		this.element = wrapper;
+
+		this.css = 'div{\n';
+		this.css += '\twidth: '+this.pixelSize+'px;\n';
+		this.css += '\theight: '+this.pixelSize+'px;\n';
+		this.css += '\tbackground: '+this.colors[0][0]+';\n';
+		this.css += '\tbox-shadow: '+boxShadowExport+';\n}';
 	}
-}
 
-function generateCss(colors, maxX, maxY) {
-	var mainPixel = $('#mainPixel');
-
-	// the first color is the background of the div
-	mainPixel.css({
-		'width' : pixelSize,
-		'height' : pixelSize,
-		'background' : colors[0][0]
-	});
-
-	var boxShadowString = '',
-		boxShadowExport = '';
-
-	// once again two nested loops. This sucks.
-	for (var x = 0; x < maxX; x++) {
-		for (var y = 0; y < maxY; y++) {
-			var boxShadow = (x * pixelSize) + 'px ' + (y * pixelSize) + 'px 0 ' + colors[x][y];
-			boxShadow += ',';
-			boxShadowString += boxShadow;
-			boxShadowExport += boxShadow+'\n\t\t\t';
+	/**
+	 *	Returns the css generated
+	 *
+	 *	@param cb Callback function
+	 */
+	this.exportCSS = function(cb) {
+		var self = this;
+		if (this.css != '') {
+			cb(this.css);
+		}
+		else {
+			this.createElement(function() {
+				cb(self.css);
+			})
 		}
 	}
-	// remove the last comma
-	boxShadowString = boxShadowString.slice(0, -1);
-	boxShadowExport = boxShadowExport.slice(0, -5);
-
-	document.querySelector('#input').style.display = 'none';
-
-	// apply the css
-	mainPixel.css('box-shadow', boxShadowString);
-
-	exportCss(boxShadowExport);
-}
-
-function exportCss(boxShadow) {
-	var css = 'div{\n';
-		css += '\twidth: '+pixelSize+'px;\n';
-		css += '\theight: '+pixelSize+'px;\n';
-		css += '\tbackground: '+$('#mainPixel').css('background-color')+';\n';
-		css += '\tbox-shadow: '+boxShadow+';\n}';
-
-	document.querySelector('textarea').innerHTML = css;
-
-	document.querySelector('#code').style.display = 'block';
 }
---
+++ app/scripts/main.js
@@ -0,0 +1,36 @@
+document.querySelector('input[type=range]').addEventListener('change', function() {
+	document.querySelector('#pixelSizeValue').innerHTML = this.value;
+});
+
+var pixelSize = document.querySelector('input[type=range]').value;
+
+document.querySelector('input[type=file]').addEventListener('change', function(e) {
+	document.querySelector('#input').innerHTML = '<p>Processing ...</p>';
+
+	// read the image without uploading it
+    var reader = new FileReader();
+    reader.onload = function(e) {
+    	var img = new Image;
+		img.src = e.target.result;
+
+		// create the object
+		var imageToCSS3 = new ImageToCSS3(img);
+
+		// set the quality
+		imageToCSS3.setQuality(pixelSize);
+
+		// get the element created
+		imageToCSS3.createElement(function(element) {
+			var output = document.querySelector('#output');
+			output.appendChild(element);
+		});
+
+
+		// get the css generated
+		imageToCSS3.exportCSS(function(css) {
+			console.log(css);
+		});
+
+    }
+    reader.readAsDataURL(this.files[0]);
+});
